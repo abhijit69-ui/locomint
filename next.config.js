d84bcbcf06cf05430ff577cd13194ef456775243
abhijit <abhijitnath1a1@gmail.com>
2023-06-08 02:24:38 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const dedicatedEndPoint = 'https://abhijit-crypto.infura-ipfs.io';
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [dedicatedEndPoint, 'abhijit-crypto.infura-ipfs.io'],
+  },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+};
+module.exports = nextConfig;
