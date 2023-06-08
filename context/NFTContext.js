0cd133860d87d741c402b7e6c4bbe77932bc4d02
abhijit <abhijitnath1a1@gmail.com>
2023-06-08 17:51:22 +0530

    modified NFTContext

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -64,6 +64,23 @@ export const NFTProvider = ({ children }) => {
       console.log('Error uploading file to IPFS.');
     }
   };
+  const createSale = async (url, formInputPrice, isReselling, id) => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const price = ethers.utils.parseUnits(formInputPrice, 'ether');
+    const contract = fetchContract(signer);
+    const listingPrice = await contract.getListingPrice();
+
+    const transaction = !isReselling
+      ? await contract.createToken(url, price, { value: listingPrice.toString() })
+      : await contract.resellToken(id, price, { value: listingPrice.toString() });
+
+    setIsLoadingNFT(true);
+    await transaction.wait();
+  };
 
   const createNFT = async (formInput, fileUrl, router) => {
     const { name, description, price } = formInput;
@@ -85,24 +102,6 @@ export const NFTProvider = ({ children }) => {
       console.log(error);
       console.log('Error uploading file to IPFS.');
     }
-  };
-
-  const createSale = async (url, formInputPrice, isReselling, id) => {
-    const web3Modal = new Web3Modal();
-    const connection = await web3Modal.connect();
-    const provider = new ethers.providers.Web3Provider(connection);
-    const signer = provider.getSigner();
-
-    const price = ethers.utils.parseUnits(formInputPrice, 'ether');
-    const contract = fetchContract(signer);
-    const listingPrice = await contract.getListingPrice();
-
-    const transaction = !isReselling
-      ? await contract.createToken(url, price, { value: listingPrice.toString() })
-      : await contract.resellToken(id, price, { value: listingPrice.toString() });
-
-    setIsLoadingNFT(true);
-    await transaction.wait();
   };
 
   const fetchNFTs = async () => {
